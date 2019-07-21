b9ba2dda2f53f02bffa0b33d08b2aeab3da25f60
PatrykKudyk <gustaw5022@gmail.com>
2019-07-21 16:16:50 +0200

    Added Patient class

---
+++ doctor-manager-working/src/main/java/com/dziobsoft/doctormanager/doctor/models/Patient.java
@@ -0,0 +1,29 @@
+package com.dziobsoft.doctormanager.doctor.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "doctor")
+public class Patient {
+    @Column(name = "id")
+    @Id
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "lastname")
+    private String lastname;
+    @Column(name = "birthDate")
+    private Date date;
+    @Column(name = "email")
+    private String email;
+}
